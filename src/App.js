766d4786de361f8f092e260018e370010856cf1a
harlyon <harrisonekpobimi@gmail.com>
2019-05-31 15:24:24 +0200

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Main from './component/Main';
+import Create from './component/Create';
+import Edit from './component/Edit';
+import Show from './component/Show';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path ="/" component={Main} />
+        <Route exact path ="/create" component={Create} />
+        <Route path='/edit/:id' component={Edit} />
+        <Route path='/show/:id' component={Show} />
+      </Router>
     </div>
   );
 }
---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+import firestore from 'firebase/firestore'
+
+const settings = {timestampsInSnapshots: true};
+
+const config = {
+  apiKey: "AIzaSyA3IUuT8bpeOpLhV7TkSwPFpe_8ecj3mjc",
+  authDomain: "project2-888db.firebaseapp.com",
+  databaseURL: "https://project2-888db.firebaseio.com",
+  projectId: "project2-888db",
+  storageBucket: "project2-888db.appspot.com",
+  messagingSenderId: "965105450373",
+  appId: "1:965105450373:web:ad2699fc8ce5b036"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings(settings);
+export default firebase;
